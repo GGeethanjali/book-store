46fdd14bc5ab373bf546b24db01c3f019c41d432
Geethanjali <geethag@thoughtworks.com>
2020-10-07 12:32:34 +0530

    Replace angular material with primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatListModule} from "@angular/material/list";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
 import {HttpClientModule} from "@angular/common/http";
 import {GoogleBooksService} from "./services/google-books";
+import { LayoutModule } from '@angular/cdk/layout';
+import { BookItemComponent } from './pages/book-item/book-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatListModule,
-    MatButtonModule,
-    MatIconModule,
-    HttpClientModule
+    HttpClientModule,
+    LayoutModule
   ],
   providers: [
     GoogleBooksService
